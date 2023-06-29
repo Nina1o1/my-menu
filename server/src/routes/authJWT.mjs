a33756e5554a5f0a14bb9de7ad7fe7577f360d79
Qiwen Xiao <nina@10-18-233-101.dynapool.wireless.nyu.edu>
2023-06-29 16:03:59 -0400

    login async compare bug

--- server/src/routes/authJWT.mjs
+++ server/src/routes/authJWT.mjs
@@ -16,7 +16,7 @@ async function loginRouter (req, res) {
     const foundUser = await User.findOne({username: username});
     if(!foundUser) return res.status(401).send({message: status["login-noexist"]});  // handle user mistakes
     
-    const compare = bcrypt.compare(password, foundUser["hash"]);
+    const compare = await bcrypt.compare(password, foundUser["hash"]);
     if(!compare) return res.status(401).send({message: status["login-noexist"]});    // handle user mistakes
 
     // create JWTs
