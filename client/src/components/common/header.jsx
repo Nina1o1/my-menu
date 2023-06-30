a58239caafb58d237def6b62a0c938c291932b38
Qiwen Xiao <nina@Qiwens-Air.lan>
2023-06-30 01:06:54 -0400

    header style adjustment

--- client/src/components/common/header.jsx
+++ client/src/components/common/header.jsx
@@ -1,42 +1,46 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './header.css';
 import Logout from "../authentication/logout";
 import useAuth from '../../hooks/useAuth';
 import terms from "../../assets/terms.json";
 function Header() {
+  const { auth } = useAuth();
   return(
-    <>
-      <div className="header">
-        <h1 className="big-heading">
-          My Menu
-        </h1>
-        <Navigation />
-      </div>
-    </>
+    <div className="header">
+      <h1 className="big-heading">My Menu</h1>
+      <Navigate />
+    </div>
   )
 }
 
-function Navigation() {
+function Navigate() {
   const { auth } = useAuth();
-
-  return(
+  return (
     <nav className="nav-container">
-      <NavLink to="/" className="nav-item">Dictionary</NavLink>
-      <NavLink to="/edit" className="nav-item">Add</NavLink>
-      <span className="nav-userName">
-        { auth?.username
-          ? ( 
-            <> 
-              {`${auth.username || "username"} | `} 
-              <Logout/>
-            </> 
-          )
-          : ( `${terms["unlogged-user"]}` )
-        }
-      </span>
-      
+        <NavLink to="/" className="nav-item">Dictionary</NavLink>
+        <NavLink to="/edit" className="nav-item">Add</NavLink>
+        <span className="nav-userName">
+          { auth?.username
+            ? ( 
+              <> 
+                {`${auth.username || "username"} | `} 
+                <Logout/>
+              </> 
+            )
+            : ( 
+              <> 
+                {`${terms["unlogged-user"]} | `}
+                <Link to="/login" className="nav-login">Login</Link>
+              </> 
+            )
+          }
+        </span>
     </nav>
   )
 }
 
 export default Header;
+
+/*
+
+*/
