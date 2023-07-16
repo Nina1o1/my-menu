11b092e8ec2f7da5ec9353626fb6bef9cf7fe8af
Qiwen Xiao <nina@Qiwens-Air.lan>
2023-07-16 00:36:52 -0400

    form data render error

--- client/src/components/edit/formBtn.jsx
+++ client/src/components/edit/formBtn.jsx
@@ -1,6 +1,4 @@
 import { useLocation, useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { addRecipe } from "../../features/recipesSlice";
 import "./edit.css";
 import { LabelContainer } from "./editComponents";
 import useAxiosTooken from "../../common/hooks/useAxiosTooken";
@@ -9,57 +7,23 @@ import useLogout from "../../common/hooks/useLogout";
 function FormBtn({formItems}) {
   // secure post request
   const axiosTookenProvider = useAxiosTooken();
-  // const dispatch = useDispatch();
-  
   // logout hooks
   const resetUserInfo = useLogout();
   const location = useLocation();
   const navigate = useNavigate();
 
-
   async function handleSubmit(evt) {
     evt.preventDefault();
-    console.log(formItems);
-    if(!formItems) return; // no input, form element is null
+    let recipeData = {};
 
-    // read form data via useRef passed as property, consistent with database, see README for database
-    const formdata = {};
-    Array.from(formItems).forEach(ele => {
-      if(!ele.id) return;
-      const idtype = ele.id.split("-");
-      switch (idtype[0]) {
-
-        case "step": {
-          if (!formdata?.["steps"]) formdata["steps"] = [];
-          if (!ele.value) return;
-          formdata["steps"].push({description: ele.value});
-          break;
-        }
-
-        case "ingredient": {
-          if (!formdata?.["ingredients"]) formdata["ingredients"] = [];
-          if (!ele.value) return;
-          const ingdtData = formdata["ingredients"];
-          if (idtype[1] === "item") {
-            ingdtData.push({item: ele.value});
-          } 
-          else if (idtype[1] === "amount") {
-            ingdtData[ingdtData.length-1]["amount"] = ele.value;
-          }
-          break;
-        }
-
-        case "categories": {
-          formdata["categories"] = [];
-          break;
-        }
-
-        default: {
-          formdata[ele.id] = ele.value;
-        }
-      }
-    });
-
+    try {
+      recipeData = readFormData(formItems); // read form data
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+    
+    
     // post request to send or update recipe
     // TODO : update recipe
     const action = "editRecipe";
@@ -71,21 +35,69 @@ function FormBtn({formItems}) {
       // post request
       await axiosTookenProvider.post(
         `/${action}`, 
-        JSON.stringify(formdata),
+        JSON.stringify(recipeData),
         postOptions
-      );
-
-    } catch (error) {
-      console.log(error);
-      resetUserInfo();
-      navigate("/login", {state: {from: location}});
+        );
+        
+      } catch (error) {
+        console.log(error);
+        resetUserInfo();
+        navigate("/login", {state: {from: location}});
+      }
     }
-  }
-
-  function handleDelete(evt) {
     
-  }
+    function handleDelete(evt) {
+      
+    }
+    
+    // read form data via useRef passed as property, consistent with database
+    function readFormData (formItems) {
+      if(!formItems) throw "no data"; // no input, form element is null
+      const formdata = {};
+      Array.from(formItems).forEach(ele => {
+        if(!ele.id) return;
+        console.log(ele);
+        const idtype = ele.id.split("-");
+        switch (idtype[0]) {
+  
+          case "step": {
+            if (!formdata?.["steps"]) formdata["steps"] = [];
+            if (!ele.value) return;
+            formdata["steps"].push({description: ele.value});
+            break;
+          }
+  
+          case "ingredient": {
+            if (!formdata?.["ingredients"]) formdata["ingredients"] = [];
+            if (!ele.value) return;
+            const ingdtData = formdata["ingredients"];
+            if (idtype[1] === "item") {
+              ingdtData.push({item: ele.value});
+            } 
+            else if (idtype[1] === "amount") {
+              ingdtData[ingdtData.length-1]["amount"] = ele.value;
+            }
+            break;
+          }
+          
+          case "categories": {
+            formdata["categories"] = [];
+            break;
+          }
 
+          case "dishname": {
+            if(!ele.value) throw "no dishname";
+          }
+  
+          default: {
+            console.log()
+            formdata[ele.id] = ele.value;
+          }
+        }
+      });
+      return formdata;
+    }
+  
   return(
     <>
         <LabelContainer />
